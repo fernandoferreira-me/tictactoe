f7ab516d7ecaf322d77b925adad5f5404fdbf46e
Fernando Ferreira <fernandogferreira@gmail.com>
2021-04-30 10:51:08 -0300

    :rotating_light: fixing code smells

--- tictactoe.py
+++ tictactoe.py
@@ -6,8 +6,6 @@ import random
 
 
 # Globals
-BOARD = dict([(idx, ' ') for idx in range(1, 10)])
-
 VICTORIES = [
     (1, 2, 3),
     (4, 5, 6),
@@ -19,52 +17,54 @@ VICTORIES = [
     (3, 6, 9)
 ]
 
-def print_board():
+# Functions
+def print_board(board):
     """
     Print the board
     """
-    print(f"{BOARD[7]}|{BOARD[8]}|{BOARD[9]}")
+    print(f"{board[7]}|{board[8]}|{board[9]}")
     print("-+-+-")
-    print(f"{BOARD[4]}|{BOARD[5]}|{BOARD[6]}")
+    print(f"{board[4]}|{board[5]}|{board[6]}")
     print("-+-+-")
-    print(f"{BOARD[1]}|{BOARD[2]}|{BOARD[3]}")
+    print(f"{board[1]}|{board[2]}|{board[3]}")
 
-def has_finished():
+def has_finished(board):
     """
     Check whether all available moves are made
     """
-    return  all([value != " " for value in BOARD.values()])
+    return  all([value != " " for value in board.values()])
 
 
-def check_sequency(positions):
+def check_sequency(positions, board):
     """
     Check if a winner sequency has been filled.
     """
-    i, j, h = positions
-    if BOARD[i] == BOARD[j] == BOARD[h] != " ":
+    idx, jdx, hdx = positions
+    if board[idx] == board[jdx] == board[hdx] != " ":
         return True
     return False
 
 
-def has_winner():
+def has_winner(board):
     """
     Check whether anyone has won
     """
-    return any([check_sequency(p) for p in VICTORIES])
+    return any([check_sequency(p, board) for p in VICTORIES])
 
 
-def reset_board():
+def reset_board(board):
     """
     Clear board positions for a new round
     """
-    global BOARD
-    for idx in BOARD.keys():
-        BOARD[idx] = ' '
+    for idx in board.keys():
+        board[idx] = ' '
+    return board
 
-def play_human():
+def play_human(**args):
     """
     Ask for human interaction
     """
+    # pylint: disable=W0613
     try:
         move = int(input("Qual a posição de sua jogada?"))
         if move not in range(1, 10):
@@ -75,56 +75,62 @@ def play_human():
         move = play_human()
     return move
 
-def play_ai():
+def play_ai(**args):
     """
     Robot pick its  move
     """
-    available_moves = [position for position, turn in BOARD.items() if turn == " "]
+    board = args.get('board')
+    available_moves = [position for position, turn in board.items()
+                       if turn == " "]
     idx = random.randint(0, len(available_moves) - 1)
     return available_moves[idx]
 
 
-PLAYERS = {
-    "X": play_human, "O": play_ai
-}
+def get_players():
+    """
+    Get the movement functions for each player
+    """
+    return {"X": play_human, "O": play_ai}
 
-def game():
+
+##
+# Main method
+def game(board):
     """
     Main function, where the game happens
     """
-    global BOARD
     turn = "X"
-    while not has_finished():
-        # Inicia a vez
+    players = get_players()
+    while not has_finished(board):
         print()
         print(f"Jogador {turn} é a sua vez")
-        move = PLAYERS[turn]()
-        print(move)
+        move = players[turn](board=board)
 
-        if BOARD[move] == ' ':
-            BOARD[move] = turn
+        if board[move] == ' ':
+            board[move] = turn
         else:
-            print ('Posição já jogada')
+            print('Posição já jogada')
             continue
 
-        print_board()
+        print_board(board)
 
-        if has_winner():
+        if has_winner(board):
             print("\n O JOGO ACABOU \n")
             print(f"***** {turn} ganhou ****")
             break
 
-        # Alterna as vezes do jogo
         if turn == "X":
             turn = "O"
         else:
             turn = "X"
 
     restart = input("Gostaria de jogar novamente? (y/n)")
-    if restart == 'y' or restart =='Y':
-        reset_board()
-        game()
+    if restart in ('y', 'Y'):
+        board = reset_board(board)
+        game(board)
 
 
+# Run as a script
 if __name__ == "__main__":
-    game()
+    theBoard = {idx: ' ' for idx in range(1, 10)}
+    game(theBoard)
